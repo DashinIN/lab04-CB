517a4cd54a856582b8498bbd37443023dea9876c
igreengrant@gmail.com <igreengrant@gmail.com>
2021-05-22 11:29:39 +0300

    curl example

--- lab03/lab03.cpp
+++ lab03/lab03.cpp
@@ -1,14 +1,32 @@
 ﻿#include <iostream>
-#include <vector> 
+#include <vector>
 #include "svg.h"
 #include "histogram.h"
-
+#include <curl/curl.h>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 
-int main() {
-   
+
+
+int main(int argc, char* argv[]) {
+
+curl_global_init(CURL_GLOBAL_ALL);
+
+  CURL* curl = curl_easy_init();
+
+if (argc > 1) {
+ CURLcode res;
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+        return 0;
+    }
+
+
+
    const auto input = read_input(cin, true);
    const auto bins = make_histogram(input);
    show_histogram_svg(bins);
